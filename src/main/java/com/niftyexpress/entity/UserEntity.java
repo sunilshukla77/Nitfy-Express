a2b087bf6cd7ae1bec430ea062aa275e0692d952
sunil <sunil.jobs77@gmail.com>
2020-11-18 20:16:05 +0530

    Data

--- src/main/java/com/niftyexpress/entity/UserEntity.java
+++ src/main/java/com/niftyexpress/entity/UserEntity.java
@@ -14,10 +14,8 @@ public class UserEntity implements Serializable {
     @Id
     private String id;
     @Indexed
-    @Field("FIRST_NAME")
-    private String firstName;
-    @Field("LAST_NAME")
-    private String lastName;
+    @Field("NAME")
+    private String name;
     @Field("GENDER")
     private String gender;
     @Field("USER_NAME")
@@ -38,12 +36,11 @@ public class UserEntity implements Serializable {
     public UserEntity() {
     	
     }
-	public UserEntity(String id, String firstName, String lastName, String gender, String userName, String password,
-			String email, String dateOfBirth, String contactNo) {
+
+	public UserEntity(String name, String gender, String userName, String password, String email, String dateOfBirth,
+			String contactNo) {
 		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
+		this.name = name;
 		this.gender = gender;
 		this.userName = userName;
 		this.password = password;
@@ -51,6 +48,146 @@ public class UserEntity implements Serializable {
 		this.dateOfBirth = dateOfBirth;
 		this.contactNo = contactNo;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((contactNo == null) ? 0 : contactNo.hashCode());
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserEntity other = (UserEntity) obj;
+		if (contactNo == null) {
+			if (other.contactNo != null)
+				return false;
+		} else if (!contactNo.equals(other.contactNo))
+			return false;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserEntity [id=" + id + ", name=" + name + ", gender=" + gender + ", userName=" + userName
+				+ ", password=" + password + ", email=" + email + ", dateOfBirth=" + dateOfBirth + ", contactNo="
+				+ contactNo + "]";
+	}
+	
+	
 	
 }
 
